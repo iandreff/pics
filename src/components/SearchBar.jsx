4947e4cd3366ec3ffd43f7b5b1357c30218c11f9
Ygor Andreff <iandreff@gmail.com>
2021-05-25 14:56:05 -0600

    added a touche of style to searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,13 @@ class SearchBar extends React.Component {
     state = {  }
     render() { 
         return (
-        <div>
-            <form action="">
-                <input type="text" name="" id="" />
-            </form>
+        <div className="ui segment">
+            <div className="field">
+                <form className="ui form" action="">
+                    <label htmlFor="">Image Search</label>
+                    <input type="text" name="" id="" />
+                </form>  
+            </div>
         </div>
         );
     }
